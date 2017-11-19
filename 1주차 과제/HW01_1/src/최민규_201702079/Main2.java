eec0e3ead2a8115d65186f0c86c6ddfcf949930a
최민규 <cmg5691@naver.com>
2017-11-19 19:53:33 +0900

    first assignment

---
+++ 1주차 과제/HW01_1/src/최민규_201702079/Main2.java
@@ -0,0 +1,19 @@
+package 최민규_201702079;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		
+		a=1;
+		System.out.println(a+". 사람들 사이에 섬이 있다.");
+		a=2;
+		System.out.println(a+". 그 섬에 가고 싶다.");
+		a=3;
+		System.out.println(a+". ");
+		a=4;
+		System.out.println(a+". -정현종-");
+	}
+
+}
